337c88ffe3c35dbc10a02e421521488102327b10
junjiezeng <zengjunj21@sina.com>
2020-05-17 14:45:29 +0800

    本地存储模拟加入购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -3,6 +3,14 @@
   -- 点击轮播图 预览大图
      给轮播图绑定点击事件
      调用小程序的apI previewImage
+  -- 点击 加入购物车
+    - 先绑定点击事件
+    - 获取缓存中的购物车数据 数组格式
+    - 先判断当前是否已经存在与购物车中
+      - 如果已经存在购物车则执行购物车数量++，重新把数组填充到缓存中
+    - 不存于与购物车的数组中，直接给购物车数组添加一个新元素，新元素带上购买数量属性 num，把数组填充到缓存中
+    - 弹出提示
+  
 
 
 */
@@ -56,6 +64,31 @@ Page({
     wx.previewImage({
       current:current,
       urls:urls
+    })
+  },
+  // 加入购物车
+  handleCartAdd(){
+    console.log("cart")
+    // 获取缓存中的购物车 数组
+    let cart = wx.getStorageSync('cart') || []
+    // 判断商品是否存在于购物车数组中
+    let index = cart.findIndex(v=>v.goods_id === this.GoodsInfo.goods_id)
+    if(index === -1){
+      // 不存在 第一次添加
+      this.GoodsInfo.num = 1
+      cart.push(this.GoodsInfo)
+    }else{
+      // 已经存在购物车 执行num++
+      cart[index].num++
+    }
+
+    // 把购物车重新添加到缓存中
+    wx.setStorageSync("cart",cart)
+    // 弹窗提示
+    wx.showToast({
+      title:'加入成功',
+      icon:'success',
+      mask:true  // ture:防止用户收到疯狂点击按钮
     })
   }
 
