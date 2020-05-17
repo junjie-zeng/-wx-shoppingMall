ca6df164dc214daec2f220d60b5fe437853d4917
junjiezeng <zengjunj21@sina.com>
2020-05-17 14:24:38 +0800

    商品详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,14 @@
+/*
+  -- 发送请求获取数据
+  -- 点击轮播图 预览大图
+     给轮播图绑定点击事件
+     调用小程序的apI previewImage
+
+
+*/
+import { request } from '../../request/index.js'
+import regeneratorRuntime from '../../utils/runtime.js'
+
 // pages/goos_list/index.js
 Page({
 
@@ -5,62 +16,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goodsObj:{}
   },
-
+  // 商品对象
+  GoodsInfo:{},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    const { goods_id } = options
+    console.log(goods_id)
+    this.getGoodsDetail(goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取商品详情
+  async getGoodsDetail(goods_id){
+    const res = await request({url:'/goods/detail',data:{goods_id}})
+    const goodsObj = res.data.message
+    this.GoodsInfo = res.data.message
+    console.log(goodsObj)
+    this.setData({
+      goodsObj:{
+        goods_name:goodsObj.goods_name,
+        goods_price:goodsObj.goods_price,
+        // iphone部分手机 不识别webp图片格式
+        // 找到后台进行修改/ 临时修改(1.webp=>1.jpg) replace替换
+        // goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
+        goods_introduce:goodsObj.goods_introduce,
+        pics:goodsObj.pics
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击轮播图 放大预览
+  handlePrevewImage(e){
+    console.log("放大预览")
+    // 先构造要预览的图片数组
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid)
+    // 接收传递过来的url
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current:current,
+      urls:urls
+    })
   }
+
+  
 })
--- request/index.js
+++ request/index.js
@@ -1,6 +1,20 @@
 
 /* 封装promise */
+
+
+// 同时发送异步代码的次数（针对一个页面同时调用多次接口）
+let ajaxTimes = 0 // 调用时++ ，请求回来时—-，直到等于0时才将图标关闭
+
 export const request = (params)=>{
+  ajaxTimes ++
+  //显示正在加载图标
+  wx.showLoading({
+    title:'加载中',
+    mask:true
+  })
+
+
+  // 公共接口
   let prefixUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
   return new Promise((resolve,reject)=>{
     wx.request({
@@ -11,6 +25,15 @@ export const request = (params)=>{
       },
       fail:(err)=>{
         reject(err)
+      },
+      complete:()=>{// 不管成功或失败都会调用
+        ajaxTimes -- 
+        if(ajaxTimes === 0){
+          // 关闭正在等待图标
+           wx.hideLoading();
+        }
+        
+
       }
     })
   })
