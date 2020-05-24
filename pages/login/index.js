6a9110df864a1067f016c506e2a82ea716397b25
junjiezeng <zengjunj21@sina.com>
2020-05-24 15:35:49 +0800

    个人中心

--- pages/login/index.js
+++ pages/login/index.js
@@ -13,54 +13,19 @@ Page({
    */
   onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  }, 
+  // 获取用户信息
+  handleGetUserInfo(e){
+    // 获取数据
+    const {userInfo} = e.detail
+    // 存储用户信息
+    wx.setStorageSync('userinfo',userInfo)
+    // 返回上一页
+    wx.navigateBack({
+      delta:1
+    })
+    
+    console.log(e)
   }
+  
 })
--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,100 @@
-// pages/order/index.js
+import { request } from '../../request/index.js'
+import regeneratorRuntime from '../../utils/runtime.js'
+/*
+  1.页面被打开的时候 onShow(页面频繁的被使用)
+    0.onShow 不同于onLoad 无法在形参上接收 options 参数
+    0.5.判断缓存中有没有token 如果没有跳转到授权页面，有直接往下进行
+    1.获取参数url参数type
+    2.根据type来决定页面标题的数组元素哪个被激活选中
+    2.根据type 去发送请求获取订单数据
+    3.页面渲染
+
+  2.点击不同的标题，重新发送请求来获取和渲染数据
+    切换tab组件，重新发送请求
+
+*/
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    orders:[],
+    tabs:[
+      {
+        id:0,
+        value:'全部',
+        isActive:true
+      },
+      {
+        id: 0,
+        value: '代付款',
+        isActive: false
+      },
+      {
+        id: 0,
+        value: '代发货',
+        isActive: false
+      },
+      {
+        id: 0,
+        value: '退款退货',
+        isActive: false
+      }
+    ],
+  },
+  onShow(optinos){
+    const token = wx.getStorageSync("token")
+    // 判断是否有token
+    if(!token){
+      wx.navigateTo({
+        url:'/pages/auth/index'
+      })
+      return 
+    }
+    
+    // 1.获取当前的小程序的页面栈-数组 长度最大是10
+    let pages = getCurrentPages()
+    // 2.数组中 索引最大的是当前页面
+    let currentPage = pages[pages.length-1]
+    // 3.获取url上的type参数
+    const type = currentPage.options.type
+    // 4.激活选中页面标题 type = 1,index =0
+    this.changeTitleByIndex(type - 1)
+    console.log(currentPage.options.type)
+    // 4.获取
+    this.getOrders(type)
+   
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 获取订单列表的方法
+  async getOrders(type){
+    const res = await request({url:'/my/orders/all',data:{type}})
+    let orders = res.data.message.orders
+    orders = orders.map(v=>({...v,create_time_cn:new Date(v.create_time * 1000).toLocaleString()}))
+    console.log(orders)
+    this.setData({
+      orders
+    })
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //根据标题的索引来激活选中 标题数组
+  changeTitleByIndex(index){
+    const { tabs } = this.data
+    // 更改
+    tabs.forEach((item, i)=>index ===i?item.isActive = true:item.isActive = false)
+    // 设置
+    this.setData({
+      tabs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 改变table项目
+  tabsItemChanges:function(e){
+    // 1.获取index
+    const { index } = e.detail
+    this.changeTitleByIndex(index)
+    // 2.重新发送请求
+    this.getOrders(index+1)
   }
+  
 })
